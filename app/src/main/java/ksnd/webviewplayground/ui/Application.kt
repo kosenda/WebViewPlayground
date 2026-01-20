ec674bdbb04e26af347f8736cfa0ea16d61ac1cd
kosenda <kkkk0630@ezweb.ne.jp>
2026-01-20 20:13:35 +0900

    feature: エラー画面追加

--- app/src/main/java/ksnd/webviewplayground/ui/Application.kt
+++ app/src/main/java/ksnd/webviewplayground/ui/Application.kt
@@ -2,6 +2,12 @@ package ksnd.webviewplayground.ui
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(tree = Timber.DebugTree())
+    }
+}
--- app/src/main/java/ksnd/webviewplayground/ui/webview/WebViewScreen.kt
+++ app/src/main/java/ksnd/webviewplayground/ui/webview/WebViewScreen.kt
@@ -1,16 +1,29 @@
 package ksnd.webviewplayground.ui.webview
 
 import android.graphics.Bitmap
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -22,14 +35,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import ksnd.webviewplayground.R
+
+private sealed interface WebViewScreenLoadingState{
+    data object Initial: WebViewScreenLoadingState
+    data object Loading: WebViewScreenLoadingState
+    data object Error: WebViewScreenLoadingState
+    data object Success: WebViewScreenLoadingState
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,6 +63,7 @@ fun WebViewScreen(
 ) {
     val context = LocalContext.current
 
+    var loadingState by remember { mutableStateOf<WebViewScreenLoadingState>(WebViewScreenLoadingState.Initial) }
     var canGoBack by remember { mutableStateOf(false) }
     var currentUrl by remember { mutableStateOf("") }
 
@@ -47,8 +72,23 @@ fun WebViewScreen(
             webViewClient = object : WebViewClient() {
                 override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
+                    loadingState = WebViewScreenLoadingState.Loading
                     canGoBack = view?.canGoBack() ?: false
                     currentUrl = url ?: ""
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    if (loadingState != WebViewScreenLoadingState.Error) {
+                        loadingState = WebViewScreenLoadingState.Success
+                    }
+                }
+
+                override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                    super.onReceivedError(view, request, error)
+                    if (request?.isForMainFrame == true) {
+                        loadingState = WebViewScreenLoadingState.Error
+                    }
                 }
             }
         }
@@ -104,5 +144,56 @@ fun WebViewScreen(
                 .fillMaxSize(),
             update = { it.loadUrl(url) }
         )
+
+        AnimatedVisibility(
+            visible = loadingState is WebViewScreenLoadingState.Error,
+            enter = EnterTransition.None,
+            exit = fadeOut(),
+        ) {
+            ErrorContent(onRetry = webView::reload)
+        }
     }
+}
+
+@Composable
+private fun ErrorContent(
+    modifier: Modifier = Modifier,
+    onRetry: () -> Unit,
+) {
+    Column (
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Text(
+            text = stringResource(R.string.error_message),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = onRetry,
+            modifier = modifier,
+            shape = RoundedCornerShape(size = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+        ) {
+            Text(
+                text = stringResource(id = R.string.retry),
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewErrorContent() {
+    ErrorContent {}
 }
